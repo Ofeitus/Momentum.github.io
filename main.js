4d3f8af04b3bea9e49b345b06fcc8673c7ffd305
Ofeitus <2008655z@gmail.com>
2021-04-17 20:36:27 +0300

    Смена фона

--- main.js
+++ main.js
@@ -1,58 +1,116 @@
 // DOM Elements
-const time = document.querySelector('.time'),
+const prevBackButton = document.querySelector('#prev-back-button'),
+    currBackButton = document.querySelector('#curr-back-button'),
+    nextBackButton = document.querySelector('#next-back-button'),
+    time = document.querySelector('.time'),
     date = document.querySelector('.date'),
     greeting = document.querySelector('.greeting'),
     name = document.querySelector('.name'),
     focus = document.querySelector('.focus');
-
-// Show Time
-function showTime() {
-    let today = new Date(),
-        hour = today.getHours(),
-        min = today.getMinutes(),
-        sec = today.getSeconds(),
-        dayOfTheWeek = today.getDay(),
-        dayOfTheMonth = today.getDate(),
-        month = today.getMonth();
-    var options = {
-        weekday: 'long',
-        day: 'numeric',
-        month: 'long'
-    };
-
-    // Output Time
-    time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
-    date.innerHTML = new Date().toLocaleString("en", options);
-
-    setTimeout(showTime, 1000);
-}
 
 // Add Zeros
 function addZero(n) {
     return (parseInt(n, 10) < 10 ? '0' : '') + n;
 }
 
+function randInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+const backgrounds = []
+let currBackgroung = (new Date()).getHours();
+let disableTrantition = false;
+
+// Массив фоновых изображений
+for (let i = 0; i < 5; i++) {
+    backgrounds.push("assets/images/night/" + addZero(randInt(1, 20)) + ".jpg");
+}
+
+for (let i = 0; i < 7; i++) {
+    backgrounds.push("assets/images/morning/" + addZero(randInt(1, 20)) + ".jpg");
+}
+ 
+for (let i = 0; i < 4; i++) {
+    backgrounds.push("assets/images/day/" + addZero(randInt(1, 20)) + ".jpg");
+}
+
+for (let i = 0; i < 8; i++) {
+    backgrounds.push("assets/images/evening/" + addZero(randInt(1, 20)) + ".jpg");
+}
+
+// Show Time
+function showTime() {
+    let today = new Date(),
+        hour = today.getHours(),
+        min = today.getMinutes(),
+        sec = today.getSeconds();
+
+    time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
+    date.innerHTML = new Date().toLocaleString("en", {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    });
+}
+
+// Установить фон
+function setBackgrount() {
+    if (!disableTrantition) {
+        document.body.style.backgroundImage = "url('" + backgrounds[currBackgroung] + "')";
+        disableTrantition = true;
+        setTimeout(function () {
+            disableTrantition = false;
+        }, 1000);
+    }
+}
+
 // Set Background and Greeting
-function setBgGreet() {
+function setGreetHourly() {
     let today = new Date(),
         hour = today.getHours();
 
     if (hour >= 5 && hour < 12) {
         // Утро
-        document.body.style.backgroundImage = "url('https://i.ibb.co/7vDLJFb/morning.jpg')";
         greeting.textContent = 'Good morning, ';
     } else if (hour >= 12 && hour < 16) {
         // День
-        document.body.style.backgroundImage = "url('https://i.ibb.co/3mThcXc/afternoon.jpg')";
         greeting.textContent = 'Good afternoon, ';
     } else if (hour >= 16 && hour < 24) {
         // Вечер
-        document.body.style.backgroundImage = "url('https://i.ibb.co/924T2Wv/eveng.jpg')";
         greeting.textContent = 'Good evening, ';
     } else {
-        document.body.style.backgroundImage = "url('https://i.ibb.co/924T2Wv/night.jpg')";
+        // Ночь
         greeting.textContent = 'Good night, ';
     }
+    currBackgroung = (new Date()).getHours();
+    setBackgrount();
+}
+
+// Предыдущий фон
+function prevBg() {
+    if (currBackgroung == 0)
+        currBackgroung = 23;
+    else
+        currBackgroung--;
+    setBackgrount();
+}
+
+// Фон в соответствии со временем
+function currBg() {
+    currBackgroung = (new Date()).getHours();
+    setBackgrount();
+}
+
+// Следующий фон
+function nextBg() {
+    if (currBackgroung == 23)
+        currBackgroung = 0;
+    else
+        currBackgroung++;
+    setBackgrount();
 }
 
 // При клике в поле ввода текст, который там был, исчезает
@@ -112,6 +170,10 @@ function setFocus(e) {
     }
 }
 
+prevBackButton.addEventListener('click', prevBg);
+currBackButton.addEventListener('click', currBg);
+nextBackButton.addEventListener('click', nextBg);
+
 name.addEventListener('click', selectName);
 name.addEventListener('keypress', setName);
 name.addEventListener('blur', getName);
@@ -121,6 +183,8 @@ focus.addEventListener('blur', getFocus);
 
 // Run
 showTime();
-setBgGreet();
+setInterval(showTime, 1000);
+setGreetHourly();
+setInterval(setGreetHourly, 60*60*1000);
 getName();
 getFocus();
